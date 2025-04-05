d9e98413653f1cbe641fa6b934319b39704a95a8
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-04-05 15:59:17 +0200

    Add general styles and navbar from former project and adapt it to ts and angular

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+isMenuOpen = false;
+
+constructor(private router:Router) {}
+
+ngOnInit(): void {}
+
+toggleMenu () {
+  this.isMenuOpen = !this.isMenuOpen;
+}
+
+@HostListener('window:scroll', [])
+onScroll() {
+  const navLogo = document.getElementById('logo');
+  const homeLogo = document.getElementById('home-logo');
+
+  if (window.scrollY > 25) {
+    navLogo?.classList.add('scrolled');
+    homeLogo?.classList.add('scrolled');
+  } else {
+    navLogo?.classList.remove('scrolled');
+    homeLogo?.classList.remove('scrolled');
+  }
+}
+
 
 }
