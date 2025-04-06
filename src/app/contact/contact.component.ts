82a932516517ca1e04555e67b740c2684d9797cb
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-04-06 11:24:54 +0200

    Add html from former project to contact component and adjust styling for form and contact layout

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +6,28 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent  implements OnInit, AfterViewInit {
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.adjustHeroHeight();
+  }
+
+  @HostListener ('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.adjustHeroHeight();
+  }
+
+  adjustHeroHeight(): void {
+    const footer = document.querySelector('footer');
+    const hero = document.querySelector('main');
+
+    if (footer && hero) {
+      const footerHeight = footer.getBoundingClientRect().height;
+      hero.style.minHeight = `calc(100vh - ${footerHeight}px)`;
+    }
+  }
+
 
 }
