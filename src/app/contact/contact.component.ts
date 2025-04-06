3414d92eb74146d63cda3856cb9a6465b5c2fbec
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-04-06 15:35:24 +0200

    Integrated emailsj functionalty to contact component / Add modal for ux feedback / Add routerLink Contact to footer

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, AfterViewInit, HostListener } from '@angular/core';
+import { environment } from '../../environments/environment';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +9,6 @@ import { Component, OnInit, AfterViewInit, HostListener } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent  implements OnInit, AfterViewInit {
-
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
@@ -29,5 +30,40 @@ export class ContactComponent  implements OnInit, AfterViewInit {
     }
   }
 
+  public sendEmail(e: Event): void {
+    e.preventDefault(); // Prevent default form submission
 
+    // Send email using EmailJS
+    emailjs
+      .sendForm(
+        environment.emailjs.serviceId, // Use the service ID from environment
+        environment.emailjs.templateId, // Use the template ID from environment
+        e.target as HTMLFormElement, // The form element
+        {
+          publicKey: environment.emailjs.publicKey,
+        }
+        
+      )
+      .then(
+        () => {
+           // Trigger Bootstrap modal using JS
+  const modalElement = document.getElementById('successModal');
+  if (modalElement) {
+    const modal = new (window as any).bootstrap.Modal(modalElement);
+    modal.show();
+
+    // Optional: Redirect after a short delay (e.g., 3s)
+    setTimeout(() => {
+      modal.hide();
+      window.location.href = '/';
+    }, 3000); }
+        },
+        (error) => {
+          alert('Ein Fehler ist passiert, bitte versuche es später.');
+          (e.target as HTMLFormElement).reset(); // Optional: reset the form
+          window.location.href = '/';
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
 }
