bc08283f4ee242b3feabcc9c7275c5f3a57680f0
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-04-06 22:43:15 +0200

    Style the quote cards with viewport animation

--- src/app/reflections/reflections.component.ts
+++ src/app/reflections/reflections.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, QueryList, ViewChildren} from '@angular/core';
 import { quotes } from '../quotes';
 import { IQuotes } from '../iquotes';
 
@@ -8,13 +8,42 @@ import { IQuotes } from '../iquotes';
   templateUrl: './reflections.component.html',
   styleUrl: './reflections.component.css'
 })
-export class ReflectionsComponent implements OnInit {
+export class ReflectionsComponent implements OnInit, AfterViewInit {
   quotes:IQuotes[] = quotes;
+  visibleStates: boolean[] = [];
 
+  @ViewChildren('cardElem') cardElems!: QueryList<ElementRef>;
 
- constructor() { }
+  constructor() {}
 
- ngOnInit(): void {
- }
- 
+  ngOnInit(): void {} 
+
+  ngAfterViewInit(): void {
+    this.updateCardRotation();
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.updateCardRotation();
+    window.addEventListener('scroll', this.updateCardRotation.bind(this));
+  }
+
+  private updateCardRotation(): void {
+    this.cardElems.forEach((cardRef: ElementRef) => {
+      const card = cardRef.nativeElement as HTMLElement;
+      const rect = card.getBoundingClientRect();
+      const topPercent = (rect.top / window.innerHeight) * 100;
+      const bottomPercent = (rect.bottom / window.innerHeight) * 100;
+
+      const isInMiddle = topPercent < 45 && bottomPercent > 40;
+
+      if (isInMiddle) {
+        card.classList.add('straight');
+        card.classList.remove('x-rotate');
+      } else {
+        card.classList.remove('straight');
+        card.classList.add('x-rotate');
+      }
+    });
+  }
 }
