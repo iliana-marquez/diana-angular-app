65401213ab5ed7f82257008c72eef266e3542245
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-04-08 00:22:11 +0200

    Add logic for audio player placeholder in profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -7,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+
+  // to track the audio element
+  @ViewChild('audio1') audioElementRef!: ElementRef<HTMLAudioElement>;
+  isPlaying = false;
+
+toggleAudio(audioElement: HTMLAudioElement, event: Event): void {
+  event.stopPropagation();
+
+  if (audioElement.paused) {
+    audioElement.play();
+    this.isPlaying = true;
+
+    audioElement.onended = () => {
+      this.isPlaying = false;
+    };
+  } else {
+    audioElement.pause();
+    this.isPlaying = false;
+  }
+}
+
+ngAfterViewInit(): void {
+  const carousel = document.getElementById('audioCarousel');
+
+  if (carousel) {
+    carousel.addEventListener('slide.bs.carousel', () => {
+      if (this.audioElementRef?.nativeElement && !this.audioElementRef.nativeElement.paused) {
+        this.audioElementRef.nativeElement.pause();
+        this.isPlaying = false;
+      }
+    });
+  }
+}
 
 }
