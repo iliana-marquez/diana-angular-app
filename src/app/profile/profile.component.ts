a1df528bd61a9dd5e2c825eb84a1f003cf621cc5
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-04-08 02:37:56 +0200

    Add functional video player in media section of profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,37 +8,56 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class ProfileComponent {
 
+// **** LOGIC FOR AUDIO MEDIA PLAYER ****
   // to track the audio element
   @ViewChild('audio1') audioElementRef!: ElementRef<HTMLAudioElement>;
   isPlaying = false;
 
-toggleAudio(audioElement: HTMLAudioElement, event: Event): void {
-  event.stopPropagation();
+  toggleAudio(audioElement: HTMLAudioElement, event: Event): void {
+    event.stopPropagation();
 
-  if (audioElement.paused) {
-    audioElement.play();
-    this.isPlaying = true;
+    if (audioElement.paused) {
+      audioElement.play();
+      this.isPlaying = true;
 
-    audioElement.onended = () => {
-      this.isPlaying = false;
-    };
-  } else {
-    audioElement.pause();
-    this.isPlaying = false;
-  }
-}
-
-ngAfterViewInit(): void {
-  const carousel = document.getElementById('audioCarousel');
-
-  if (carousel) {
-    carousel.addEventListener('slide.bs.carousel', () => {
-      if (this.audioElementRef?.nativeElement && !this.audioElementRef.nativeElement.paused) {
-        this.audioElementRef.nativeElement.pause();
+      audioElement.onended = () => {
         this.isPlaying = false;
-      }
-    });
+      };
+    } else {
+      audioElement.pause();
+      this.isPlaying = false;
+    }
   }
-}
+
+  // to track the audio element
+  ngAfterViewInit(): void {
+    const carousel = document.getElementById('audioCarousel');
+
+    if (carousel) {
+      carousel.addEventListener('slide.bs.carousel', () => {
+        if (this.audioElementRef?.nativeElement && !this.audioElementRef.nativeElement.paused) {
+          this.audioElementRef.nativeElement.pause();
+          this.isPlaying = false;
+        }
+      });
+    }
+  }
+
+// **** LOGIC FOR VIDEO PLAYER 
+  // To handle modal state and video url
+  selectedVideoUrl: string | null = null;
+  showVideoModal = false;
+
+  // Opens the video modal with the selected video URL
+  openVideoModal(videoUrl: string): void {
+    this.selectedVideoUrl = videoUrl;
+    this.showVideoModal = true;
+  }
+
+  // Closes the video modal
+  closeVideoModal(): void {
+    this.selectedVideoUrl = null;
+    this.showVideoModal = false;
+  }
 
 }
